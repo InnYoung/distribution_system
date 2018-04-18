ef3daad644dc21c0fdf7a3e18165ae37b80c7ffd
SilverYoung <yytbt1@foxmail.com>
2018-04-19 00:04:29 +0800

    添加同步的ignore列表，导入和使用unittest

--- test_suits/functional_tests.py
+++ test_suits/functional_tests.py
@@ -3,11 +3,28 @@
 
 from selenium import webdriver
 from time import sleep
+import unittest
 
-browser = webdriver.Chrome()
-browser.get("http://localhost:8000")
-assert "Django" in browser.title
+class NewVisitorTest(unittest.TestCase):
 
-sleep(20)
-browser.close()
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_get_login_web(self):
+        self.browser.get("http://localhost:8000")
+
+        # 验证首页title包含Tiger
+        self.assertIn('Tiger', self.browser.title)
+        self.fail('finish the test')
+
+
+
+# 登录失败
+
+# 登录成功，跳转到用户中心界面
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
